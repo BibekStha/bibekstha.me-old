aab6f922eaa8ea1d76e9bdff864ba68260a7ef4b
Bibek <bibekmanshrestha@gmail.com>
2019-05-22 12:58:02 -0400

    add lins to projects

--- src/components/projects/DotaStats.vue
+++ src/components/projects/DotaStats.vue
@@ -6,18 +6,28 @@
         <small>[ Individual ]</small>
         <v-layout row wrap pt-2>
           <v-flex xs5>
-            <img src="../../assets/dotastats.gif" alt="" srcset="" class="project_gifs">
+            <a href="http://dotastats.ga" target="_blank">
+              <img
+                src="../../assets/dotastats.gif"
+                alt=""
+                srcset=""
+                class="project_gifs"
+              />
+            </a>
           </v-flex>
 
           <v-flex xs7 project_detail>
-            This website is about Dota 2 game, the game I'm big fan of. Visitors can come to this website 
-            and look at real time different stats of the game like top players and teams. 
-            Users can login using Steam id to get details about their own performance in the game.
-            <br><br>
-            In future, they can also view details of major tournaments. I have plans to add machine learning to help users understand their statistics
-             and know ways to improve their game.
-            <br><br>
-            I used Laravel framework for this website and utilized few APIs like Steam API and OpenDota API and some other useful libraries.
+            This website is about Dota 2 game, the game I'm big fan of. Visitors
+            can come to this website and look at real time different stats of
+            the game like top players and teams. Users can login using Steam id
+            to get details about their own performance in the game.
+            <br /><br />
+            In future, they can also view details of major tournaments. I have
+            plans to add machine learning to help users understand their
+            statistics and know ways to improve their game.
+            <br /><br />
+            I used Laravel framework for this website and utilized few APIs like
+            Steam API and OpenDota API and some other useful libraries.
           </v-flex>
         </v-layout>
       </v-flex>
@@ -28,8 +38,7 @@
 <script>
 export default {
   name: "dotastats"
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/projects/GetTogether.vue
+++ src/components/projects/GetTogether.vue
@@ -6,19 +6,30 @@
         <small>[ Group ]</small>
         <v-layout row wrap pt-2>
           <v-flex xs5>
-            <img src="../../assets/gettogether.gif" alt="" srcset="" class="project_gifs">            
+            <a href="http://dotastats.ga" target="_blank">
+              <img
+                src="../../assets/gettogether.gif"
+                alt=""
+                srcset=""
+                class="project_gifs"
+              />
+            </a>
           </v-flex>
 
           <v-flex xs7 project_detail>
-            We redesigned a hospital website from group up and created content management system for the admins 
-            and hospital staffs to manage website content. It was built on ASP.NET Core 2.0.
-            <br><br>
-            Among three features I built, donation feature is the one I'm proud of. I implemented the feature with 
-            PayPal API using the official sdk provided by their team. Even though it was official sdk, I had to debug 
-            the code in several steps to make the program work which involved considerable amount of my time. 
-            In the end, it all worked out.
-            <br><br>
-            I also worked as repository admin and front end developer for the website.
+            We redesigned a hospital website from group up and created content
+            management system for the admins and hospital staffs to manage
+            website content. It was built on ASP.NET Core 2.0.
+            <br /><br />
+            Among three features I built, donation feature is the one I'm proud
+            of. I implemented the feature with PayPal API using the official sdk
+            provided by their team. Even though it was official sdk, I had to
+            debug the code in several steps to make the program work which
+            involved considerable amount of my time. In the end, it all worked
+            out.
+            <br /><br />
+            I also worked as repository admin and front end developer for the
+            website.
           </v-flex>
         </v-layout>
       </v-flex>
@@ -29,8 +40,7 @@
 <script>
 export default {
   name: "sensenhumber"
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
